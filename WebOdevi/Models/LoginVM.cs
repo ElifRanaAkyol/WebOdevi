31e614875f494cb5f4cc1e023e62504a4762c465
Rana <akyolranaelif@gmail.com>
2025-12-12 23:43:31 +0300

    5. commit

--- WebOdevi/Models/LoginVM.cs
+++ WebOdevi/Models/LoginVM.cs
@@ -7,9 +7,11 @@ namespace WebOdevi.Models.ViewModels
         [Required(ErrorMessage = "Bu alan boş bırakılamaz!")]
         [EmailAddress(ErrorMessage = "Geçerli mail adresi giriniz!")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Bu alan boş bırakılamaz!")]
         [DataType(DataType.Password)]
-
         public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
     }
 }
